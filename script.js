ecb2a99d76a94112bfd014445dd6bb6bedef7442
Jessica David <89813821+jed06@users.noreply.github.com>
2023-05-21 14:17:59 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  var timer = 0;
-  var timerElement = document.getElementById("timer");
-
-  setInterval(() => {
-    timer += 1 / 250;
-    timerElement.textContent = parseFloat(timer.toFixed(10));
-  }, 1);
-});
