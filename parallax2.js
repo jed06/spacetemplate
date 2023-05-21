9d31d0bd53c1d9aae85c02478bf26b664baaf2ed
Jessica David <89813821+jed06@users.noreply.github.com>
2023-05-21 13:20:56 -0700

    Update parallax2.js

--- parallax2.js
+++ parallax2.js
@@ -1,5 +1,6 @@
 const imgs = document.getElementByClassName("parallax-child")
 console.log(imgs)
+console.log(1)
 imgs.forEach((image,i)=>{
 	image.addEventListner('mouseenter',()=>{
 		if(image.hasChild() == False){
