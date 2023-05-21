8606329695d24595a13bdabe220fb0178875d13c
cwbbebop <109786302+cwbbebop@users.noreply.github.com>
2023-05-20 17:42:54 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -13,8 +13,8 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  var width = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / width) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }  
   console.log("DOM fully loaded and parsed");
