599a6e4d183bc4c02520fefe6640c6419608d158
cwbbebop <109786302+cwbbebop@users.noreply.github.com>
2023-05-21 11:42:56 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -37,13 +37,15 @@ document.addEventListener("DOMContentLoaded", (event)=> {
 clearInterval(timer);
 });
 });
+  console.log("DOM fully loaded and parsed");
+});
 window.onscroll = function() {myFunction()};
 
+document.addEventListener("DOMContentLoaded", (event)=> {
 function myFunction() {
   var winScroll = document.body.scrollTop document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
-  console.log("DOM fully loaded and parsed");
-});
+}
