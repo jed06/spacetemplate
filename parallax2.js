039e06ee9e18b903378d8372c55ad3521947e2aa
Jessica David <89813821+jed06@users.noreply.github.com>
2023-05-21 13:27:49 -0700

    Update parallax2.js

--- parallax2.js
+++ parallax2.js
@@ -2,6 +2,7 @@ const imgs = document.getElementsByClassName("parallax-child")
 console.log(imgs[0])
 console.log(1)
 Array.prototype.forEach.call(imgs,(image,i)=>{
+	console.log("check")
 	image.addEventListner('mouseenter',()=>{
 		if(image.hasChild() == False){
 			console.log('mouse enter activated')
