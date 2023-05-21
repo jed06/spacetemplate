b335ed3e471443c4c2f39f59d95236656f40ad71
complying <87457990+complying@users.noreply.github.com>
2023-05-21 14:30:07 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   setInterval(() => {
     timer += 1 / 250;
     timerElement.textContent = parseFloat(timer.toFixed(10));
-    distanceElement.textContent = parseFloat(timer.toFixed(10)*speeds[index]);
+    distanceElement.textContent = parseFloat(timer.toFixed(0)*speeds[index]);
   }, 1);
   
   
