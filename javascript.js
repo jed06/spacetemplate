db03c52bd0f1a18416c612c74eeabd00cb8a008a
cwbbebop <109786302+cwbbebop@users.noreply.github.com>
2023-05-21 10:35:56 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -40,7 +40,7 @@ clearInterval(timer);
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var winScroll = document.body.scrollTop document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
