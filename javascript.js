05949020ff6d3814d755a991ce04128cdefda9d8
complying <87457990+complying@users.noreply.github.com>
2023-05-20 20:41:39 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,41 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   
 
+  
+(function () {
+    if (!console) {
+        console = {};
+    }
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
+    }
+})();
+
+console.log('test');
+console.log({
+    test: 2
+});
+console.log([1, 2, 3]);
+console.log([{
+    test: 1
+}, {
+    test: 2
+}]);
+
+document.addEventListener("DOMContentLoaded", (event)=> {
   var timer = 0;
   setInterval(() => {
     timer += 1/100; // how much to add every 10ms
     console.log(parseFloat(timer.toFixed(10)));
   }, 10);
-
+clearInterval(timer);
 });
-  
 
   console.log("DOM fully loaded and parsed");
 });
