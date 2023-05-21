07ec3055d510b541b544e1449bc05ff9be556508
Jessica David <89813821+jed06@users.noreply.github.com>
2023-05-21 13:16:39 -0700

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -1,16 +1,4 @@
-const imgs = document.querySelectorAll("img")
-imgs.forEach((image,i)=>{
-	image.addEventListner('mouseenter',()=>{
-		if(image.hasChild() == False){
-			let temp = document.createElement('p')
-			temp.innerHTML = 'sample description'
-			image.appendChild(temp)
-		}
-	})
-	image.addEventListner('mouseleave',()=>{
-		image.removeChild()
-	})
-})
+
 $(function () {
   
 		var controller = new ScrollMagic.Controller(); // 1. create a scroll container
